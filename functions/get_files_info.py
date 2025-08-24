0ca2844a44a8b6a4a6ad175798b8a1dc0f3e238c
Ziloden <ziloden@pm.me>
2025-08-24 14:33:40 -0700

    Fix get files info working dir

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,14 +1,15 @@
 import os
 
 def get_files_info(working_directory, directory="."):
-    combined_path = os.path.join(working_directory, directory)
+    abs_working_dir = os.path.abspath(working_directory)
+    abs_file_path = os.path.abspath(os.path.join(working_directory, directory))
     files = []
-    if not os.path.abspath(combined_path).startswith(os.path.abspath(working_directory)):
+    if not os.path.abspath(abs_file_path).startswith(os.path.abspath(working_directory)):
         return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
-    if not os.path.isdir(combined_path):
+    if not os.path.isdir(abs_file_path):
         return f'Error: "{directory}" is not a directory'
-    for file in os.listdir(combined_path):
-        full_path = os.path.join(combined_path, file)
+    for file in os.listdir(abs_file_path):
+        full_path = os.path.join(abs_file_path, file)
         size = os.path.getsize(full_path)
         is_dir = os.path.isdir(full_path)
         file_string = f" - {file}: file_size={size}, is_dir={is_dir}"
