08deff69c5e4ad537e60a10f8c1021b78dc5cdea
Ziloden <ziloden@pm.me>
2025-08-24 08:32:19 -0700

    Change variable names

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,14 +1,14 @@
 import os
 
 def get_files_info(working_directory, directory="."):
-    path = os.path.join(working_directory, directory)
+    combined_path = os.path.join(working_directory, directory)
     files = []
-    if not os.path.abspath(path).startswith(os.path.abspath(working_directory)):
+    if not os.path.abspath(combined_path).startswith(os.path.abspath(working_directory)):
         return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
-    if not os.path.isdir(path):
+    if not os.path.isdir(combined_path):
         return f'Error: "{directory}" is not a directory'
-    for file in os.listdir(path):
-        full_path = os.path.join(path, file)
+    for file in os.listdir(combined_path):
+        full_path = os.path.join(combined_path, file)
         size = os.path.getsize(full_path)
         is_dir = os.path.isdir(full_path)
         file_string = f" - {file}: file_size={size}, is_dir={is_dir}"
