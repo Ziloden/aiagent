858df83f352220e9923b3e4db5f0204e6818201b
Ziloden <ziloden@pm.me>
2025-08-24 14:29:16 -0700

    Fix working dir

--- functions/write_file.py
+++ functions/write_file.py
@@ -1,12 +1,13 @@
 import os
 
 def write_file(working_directory, file_path, content):
-    combined_path = os.path.join(working_directory, file_path)
-    path_up_to_file = os.path.split(combined_path)[0]
-    if not combined_path.startswith(working_directory):
+    abs_working_dir = os.path.abspath(working_directory)
+    abs_file_path = os.path.abspath(os.path.join(working_directory, file_path))
+    path_up_to_file = os.path.split(abs_file_path)[0]
+    if not abs_file_path.startswith(abs_working_dir):
         return f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory'
     if not os.path.exists(path_up_to_file):
         os.makedirs(path_up_to_file)
-    with open(combined_path, "w") as f:
+    with open(abs_file_path, "w") as f:
         f.write(content)
-    return f'Successfully wrote to "{combined_path}" ({len(content)} characters written)'
+    return f'Successfully wrote to "{abs_file_path}" ({len(content)} characters written)'
