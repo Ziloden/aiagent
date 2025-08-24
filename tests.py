55a8e8363ac5a8e62314166f085be9ce55d9a198
Ziloden <ziloden@pm.me>
2025-08-24 13:14:13 -0700

    Add file read

--- tests.py
+++ tests.py
@@ -1,10 +1,20 @@
 from functions.get_files_info import get_files_info
+from functions.get_file_content import get_file_content
 
-tests = [
+file_info_tests = [
     ("Result for current directory:", "calculator", "."),
     ("Result for 'pkg' directory:", "calculator", "pkg"),
     ("Result for '/bin' directory:", "calculator", "/bin"),
     ("Result for '../' directory:", "calculator", "../"),
     ]
-for test in tests:
-    print(f"{test[0]}\n{get_files_info(*test[1:])}")
+#for test in file_info_tests:
+#   print(f"{test[0]}\n{get_files_info(*test[1:])}")
+
+file_content_tests = [
+    ("calculator", "main.py"),
+    ("calculator", "pkg/calculator.py"),
+    ("calculator", "/bin/cat"),
+    ("calculator", "pkg/does_not_exist.py"),
+]
+for test in file_content_tests:
+    print(get_file_content(*test))
