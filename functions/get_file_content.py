2fe88ea7cd5b2180156b9df0699cc6df53133601
Ziloden <ziloden@pm.me>
2025-08-24 13:16:10 -0700

    Remove unused truncate variable

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -3,7 +3,6 @@ from functions.config import MAX_CHARS
 
 def get_file_content(working_directory, file_path):
     combined_path = os.path.join(working_directory, file_path)
-    file_truncated = False
     if not os.path.abspath(combined_path).startswith(os.path.abspath(working_directory)):
         return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
     if not os.path.isfile(combined_path):
