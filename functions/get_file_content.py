7a5325441fc21d06f576e63142ac25daa399e30b
Ziloden <ziloden@pm.me>
2025-08-24 14:31:40 -0700

    Fix get file content working dir

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -2,14 +2,15 @@ import os
 from functions.config import MAX_CHARS
 
 def get_file_content(working_directory, file_path):
-    combined_path = os.path.join(working_directory, file_path)
-    if not os.path.abspath(combined_path).startswith(os.path.abspath(working_directory)):
+    abs_working_dir = os.path.abspath(working_directory)
+    abs_file_path = os.path.abspath(os.path.join(working_directory, file_path))
+    if not os.path.abspath(abs_file_path).startswith(os.path.abspath(working_directory)):
         return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
-    if not os.path.isfile(combined_path):
+    if not os.path.isfile(abs_file_path):
         return f'Error: File not found or is not a regular file: "{file_path}"'
-    with open(combined_path, "r") as f:
+    with open(abs_file_path, "r") as f:
         file_content_string = f.read(MAX_CHARS)
-        if len(file_content_string) < os.path.getsize(combined_path):
+        if len(file_content_string) < os.path.getsize(abs_file_path):
             file_content_string += f'[...File "{file_path}" truncated at 10000 characters]'
     
     return file_content_string
