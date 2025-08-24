b74c7b4a510c1d103a8807d927bd00759632c4ff
Ziloden <ziloden@pm.me>
2025-08-23 20:48:51 -0700

    Add verbose mode

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sys
 
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -13,15 +14,25 @@ def main():
         print("You must provide a prompt.")
         exit(1)
     
-    prompt = sys.argv[1]
+    user_prompt = sys.argv[1]
+    verbose = False
+    if len(sys.argv) == 3:
+        if sys.argv[2] == "--verbose":
+            verbose = True
+
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
-        contents=prompt
+        contents=messages,
     )
 
     print(response.text)
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if verbose:
+        print(f"User prompt: {user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 
 if __name__ == "__main__":
