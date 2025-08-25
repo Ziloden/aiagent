010c58f799fd21f3564c8dbff3d989e7dd308222
Ziloden <ziloden@pm.me>
2025-08-24 19:02:38 -0700

    Add run python file function definition

--- functions/run_python_file.py
+++ functions/run_python_file.py
@@ -2,6 +2,7 @@ import os
 import subprocess
 
 from functions.config import PROCESS_TIMEOUT
+from google.genai import types
 
 def run_python_file(working_directory, file_path, args=[]):
     abs_working_dir = os.path.abspath(working_directory)
@@ -22,3 +23,25 @@ def run_python_file(working_directory, file_path, args=[]):
     if process.returncode != 0:
         result_string += f"\nProcess exited with code {process.returncode}"
     return result_string
+
+schema_run_python_file = types.FunctionDeclaration(
+    name="run_python_file",
+    description="Executes the specified python file and included arguments, constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="The python file to execute, relative to the working directory. This parameter must be provided.",
+            ),
+            "args": types.Schema(
+                type=types.Type.ARRAY,
+                description="The list of arguments to be passed to the python file when executed. If this is not provided the file will be executed with no arguments.",
+                items=types.Schema(
+                    type=types.Type.STRING,
+                    description="The individual arguments to be passed to the python file when executed."
+                )
+            ),
+        },
+    ),
+)
